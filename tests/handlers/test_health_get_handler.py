c2c4112da60e52e29edf65ee57138b907041c5e5
dadabricks <55216120+dadabricks@users.noreply.github.com>
2023-03-15 15:21:54 -0700

    Update test_health_get_handler.py

--- tests/handlers/test_health_get_handler.py
+++ tests/handlers/test_health_get_handler.py
@@ -13,10 +13,10 @@ class TestHealthHandler(TestHandlerBaseClass):
 #         response = request.urlopen(url=example_url)
 #         assert response.headers.get(DB_VERSION_HEADER_KEY) == ProxyConfig.VERSION
 
-    def test_health_endpoint(self):
-        health_url = self.to_proxy_url(HEALTH_PATH)
-        response = request.urlopen(url=health_url)
-        # parse JSON response to object
-        response_data = json.loads(response.read().decode("utf-8"))
-        assert response_data["status"] == "ok"
-        assert response_data["version"] == ProxyConfig.VERSION
+#     def test_health_endpoint(self):
+#         health_url = self.to_proxy_url(HEALTH_PATH)
+#         response = request.urlopen(url=health_url)
+#         # parse JSON response to object
+#         response_data = json.loads(response.read().decode("utf-8"))
+#         assert response_data["status"] == "ok"
+#         assert response_data["version"] == ProxyConfig.VERSION
