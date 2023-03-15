07669b1eca5322b62b7ba3a4b266278eedbd56f8
Benjamin Chiang <benchiang@outlook.com>
2023-03-15 15:24:16 -0700

    disable health endpoint

--- tests/handlers/test_health_get_handler.py
+++ tests/handlers/test_health_get_handler.py
@@ -8,15 +8,16 @@ from tests.test_base_cls import TestHandlerBaseClass
 
 
 class TestHealthHandler(TestHandlerBaseClass):
-#     def test_version_header(self):
-#         example_url = self.to_proxy_url("https://example.com/")
-#         response = request.urlopen(url=example_url)
-#         assert response.headers.get(DB_VERSION_HEADER_KEY) == ProxyConfig.VERSION
-
-#     def test_health_endpoint(self):
-#         health_url = self.to_proxy_url(HEALTH_PATH)
-#         response = request.urlopen(url=health_url)
-#         # parse JSON response to object
-#         response_data = json.loads(response.read().decode("utf-8"))
-#         assert response_data["status"] == "ok"
-#         assert response_data["version"] == ProxyConfig.VERSION
+    pass
+    # def test_version_header(self):
+    #     example_url = self.to_proxy_url("https://example.com/")
+    #     response = request.urlopen(url=example_url)
+    #     assert response.headers.get(DB_VERSION_HEADER_KEY) == ProxyConfig.VERSION
+    #
+    # def test_health_endpoint(self):
+    #     health_url = self.to_proxy_url(HEALTH_PATH)
+    #     response = request.urlopen(url=health_url)
+    #     # parse JSON response to object
+    #     response_data = json.loads(response.read().decode("utf-8"))
+    #     assert response_data["status"] == "ok"
+    #     assert response_data["version"] == ProxyConfig.VERSION
